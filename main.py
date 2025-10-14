a6da28855b94f85ecb0270459d5a7791aeb7a88e
bfn <benjamin@noessler.at>
2025-10-14 19:18:43 +0200

    added asteroids and player movement

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
-import pygame;
+from tokenize import PlainToken
+import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from AsteroidField import AsteroidField
+
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
     pygame.init()
-    
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+
     player = Player(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
-    
-    
+    asteroidField = AsteroidField()
+
     running = True
     while True:
         screen.fill("black")
-        player.draw(screen)
+        dt = clock.tick(60) / 1000
+        for entity in updatable:
+            entity.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000
-    
+
 
 if __name__ == "__main__":
     main()
